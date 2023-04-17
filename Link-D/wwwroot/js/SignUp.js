33cff350a2166f0d3d321c31e802b4334e30a856
rajiv.singh4 <rajiv.singh4@aon.com>
2023-04-17 18:46:28 +0530

    Implemented signup with proper validation

--- Link-D/wwwroot/js/SignUp.js
+++ Link-D/wwwroot/js/SignUp.js
@@ -1,6 +1,7 @@
 ﻿
 function signUp() {
-    $(".error-container").attr("hidden", "true");
+    $(".empty-field").attr("hidden", "true");
+    $(".invalid-text").attr("hidden", "true");
 
     var fname = $("#firstName").val();
     var lname = $("#lastName").val();
@@ -9,45 +10,7 @@ function signUp() {
     var confirmPassword = $("#confirmpassword").val();
 
 
-    var namePattern = /^[a-zA-Z]+$/;
-    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    var passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-    var isValid = true;
-
-    if (fname.length == 0) {
-        showError("#firstName", "Please enter a valid first name.");
-        isValid = false;
-    }
-    else if (!namePattern.test(fname)) {
-        showError("#firstName", "Name must contain only letters.");
-        isValid = false;
-    }
-
-    if (lname.length == 0) {
-        showError("#lastName", "Please enter a valid last name.");
-        isValid = false;
-    }
-    else if (!namePattern.test(lname)) {
-        showError("#lastName", "Name must contain only letters.");
-        isValid = false;
-    }
-
-    if (!emailPattern.test(email)) {
-        showError("#email", "Email must contain @");
-        isValid = false;
-    }
-
-    if (password.length < 8 && !passwordPattern.test(password)) {
-        $("#pwderror").removeAttr("hidden");
-        isValid = false;
-    }
-
-    if (password != confirmPassword) {
-        $("#confirmpass").removeAttr("hidden");
-        isValid = false;
-
-    }
+    var isValid = checkValidity(fname, lname, email, password, confirmPassword);
 
     var formData = {
         FName: fname,
@@ -72,6 +35,57 @@ function signUp() {
     }
 }
 
-function showError(inputCtrlId, msg) {
-    $(inputCtrlId).closest("div").find(".error-container").removeAttr("hidden").find(".error-txt").text(msg);
+function showError(inputCtrlId,text)
+{
+    if (text.length == 0)
+        $(inputCtrlId).next().find(".empty-field").removeAttr("hidden");
+    else
+        $(inputCtrlId).next().find(".invalid-text").removeAttr("hidden");;
+}
+
+function checkValidity(fname, lname, email, password, confirmPassword)
+{
+
+    var isValid = true;
+    var namePattern = /^[a-zA-Z]+$/;
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    var passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
+    var isValid = validator("#firstName",fname, namePattern);
+    var isValid = validator("#lastName",lname, namePattern);
+    var isValid = validator("#email",email, emailPattern);
+
+
+    
+    if (password.length < 8 && !passwordPattern.test(password)) {
+        $("#password").next().removeAttr("hidden");
+        isValid = false;
+        }
+
+    if (password != confirmPassword) {
+        $("#confirmpassword").next().removeAttr("hidden");
+        isValid = false;
+
+    }
+
+    return isValid;
+
+}
+
+function validator(inputCtrlId,text,pattern)
+{
+    
+    var isValid = true;
+
+    if (text.length == 0) {
+        showError(inputCtrlId,text);
+        isValid = false;
+    }
+    else if (!pattern.test(text)) {
+        showError(inputCtrlId,text);
+        isValid = false;
+    }
+
+    return isValid;
 }
