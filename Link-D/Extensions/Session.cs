e109127e8bae1de70058000213632af48e5a4ed3
rajiv <rajiv.singh@aas.services>
2023-04-28 11:13:06 +0530

    Adding extension methods to get and set user id

---
+++ Link-D/Extensions/Session.cs
@@ -0,0 +1,22 @@
+﻿using Link_D.Models.Data;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Link_D.Extensions
+{
+    public static class Session
+    {
+
+        #region keys
+
+        public static readonly string User_Id = "User_Id";
+
+
+        #region UserId
+
+        public static void SetUserId(this ISession session, int val)
+        {
+            session.Set(User_Id, val);
+        }
+
+    }
+}
