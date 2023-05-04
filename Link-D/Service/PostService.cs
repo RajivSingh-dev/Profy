40570d812374948ba0651824903a6118e553b8c5
rajiv <rajiv.singh@aas.services>
2023-05-04 16:16:01 +0530

    implementing post save feature

--- Link-D/Service/PostService.cs
+++ Link-D/Service/PostService.cs
@@ -1,11 +1,24 @@
-﻿namespace Link_D.Service
+﻿using Link_D.Models.Data;
+
+namespace Link_D.Service
 {
     public class PostService : IPostService
     {
-   
-        public void SavePost()
-        {
+        private ProjectContext _projectContext;
 
+        public PostService(ProjectContext projectContext)
+        { 
+        _projectContext = projectContext;
+        }
+
+   
+        public void SavePost(int userId, string description)
+        {
+            Post post = new Post();
+            post.UserId = userId;
+            post.Description = description;
+            _projectContext.post.Add(post);
+            _projectContext.SaveChanges();
 
 
         }
